a75a60bb13f131383cf39fb73d52a6592fbe67de
oorsuii <2653701008@qq.com>
2020-03-23 22:28:09 -0500

    第二次提交

--- demo01.py
+++ demo01.py
@@ -64,9 +64,9 @@ print("input获取的内容：",a+b)
 # print("b的长度：",b)
 # print("字段长度和",a+b)
 
-# a = len(input("第一段内容："))
-# b = len(input("第一段内容："))
-# print("字段长度和：",a+b)
+a = len(input("第一段内容："))
+b = len(input("第一段内容："))
+print("字段长度和：",a+b)
 
 
 
